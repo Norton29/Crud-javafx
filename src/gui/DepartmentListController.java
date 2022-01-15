dc1dae7631fa26caa22e48da9505048dd9261f9b
Norton29 <norton290194@gmail.com>
2022-01-15 04:55:36 -0300

    Department Object to Department Form

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -49,7 +49,8 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private void onNewButtonAction(ActionEvent event) {
 		Stage parentStage = Utils.currentStage(event);
-		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
+		Department obj = new Department();
+		createDialogForm(obj, "/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	public void setDepartmentService(DepartmentServices services){
@@ -76,11 +77,15 @@ public class DepartmentListController implements Initializable {
 		tableViewDep.setItems(obsList);
 	}
 	
-	public void createDialogForm(String absoluteName, Stage parentStage) 
+	public void createDialogForm(Department obj, String absoluteName, Stage parentStage) 
 	{
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
+			
+			DepartmentFormController controller = loader.getController();
+			controller.setDepartment(obj);
+			controller.updateFormData();
 			
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Enter Department Data");
